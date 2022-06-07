e3385c196abdd2d28e6c55669bed235b5d9f2bdd
Chalkk <946921362@qq.com>
2022-06-07 18:50:13 -0400

    fix the bug of feed for showing the IsFavorite of video

--- controller/feed.go
+++ controller/feed.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
@@ -19,6 +21,39 @@ type FeedResponse struct {
 // need to consider to increase performance by concurrency
 func Feed(c *gin.Context) {
 	token, exist := c.GetQuery("token")
+	fmt.Println(token)
+	fmt.Println(exist)
+	//err := Db.Table("videos").Order("id DESC").Limit(30).Find(&videoList).Error
+	//if err != nil {
+	//	c.JSON(http.StatusOK, FeedResponse{
+	//		Response:  Response{StatusCode: 1, StatusMsg: err.Error()},
+	//		VideoList: nil,
+	//		NextTime:  time.Now().Unix(),
+	//	})
+	//	return
+	//}
+	//if exist {
+	//	var video *Video
+	//	var userFavoriteinfo UserFavoriteInfo
+	//	for i, _ := range videoList {
+	//		video = &videoList[i]
+	//		err = Db.Table("user_favorite_infos").
+	//			Where("token = ?", token).
+	//			Limit(1).
+	//			Find(&userFavoriteinfo).
+	//			Error
+	//		if err != nil {
+	//			c.JSON(http.StatusOK, FeedResponse{
+	//				Response:  Response{StatusCode: 1, StatusMsg: err.Error()},
+	//				VideoList: nil,
+	//				NextTime:  time.Now().Unix(),
+	//			})
+	//			return
+	//		}
+	//		video.IsFavorite = true
+	//	}
+	//}
+
 	Db.Transaction(func(tx *gorm.DB) error {
 
 		err := tx.Table("videos").Order("id DESC").Limit(30).Find(&videoList).Error
@@ -27,16 +62,24 @@ func Feed(c *gin.Context) {
 		}
 		if exist {
 			var video *Video
+			var userFavoriteinfo UserFavoriteInfo
 			for i, _ := range videoList {
 				video = &videoList[i]
 				err = tx.Table("user_favorite_infos").
-					Select("video_id").
-					Where("token = ? AND video_id = ?", token, video.Id).
-					Limit(1).Error
-				if err != nil {
+					Where("token = ?", token).
+					Where("video_id = ?", video.Id).
+					Limit(1).
+					Find(&userFavoriteinfo).
+					Error
+				if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+					fmt.Println(err.Error())
 					return err
 				}
-				video.IsFavorite = true
+				// in case the table is empty, we need check whether the query returns empty struct
+				// if the record could be found in the userFavoriteInfos, we can see the video.IsFavorite to true
+				if err == nil && userFavoriteinfo.VideoId != 0 {
+					video.IsFavorite = true
+				}
 			}
 		}
 		c.JSON(http.StatusOK, FeedResponse{
@@ -46,4 +89,10 @@ func Feed(c *gin.Context) {
 		})
 		return nil
 	})
+
+	//c.JSON(http.StatusOK, FeedResponse{
+	//	Response:  Response{StatusCode: 0},
+	//	VideoList: videoList,
+	//	NextTime:  time.Now().Unix(),
+	//})
 }
